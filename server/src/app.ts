12143b82d17a6b9a3c6489ac5119d363e70ca3e5
sehee <seeeh0128@gmail.com>
2022-09-24 22:25:29 +0900

    client와 server 초기설정

---
+++ server/src/app.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response, NextFunction } from "express";
+import cors from "cors";
+
+const app = express();
+
+// CORS 에러 방지
+app.use(
+  cors({
+    origin: true,
+    credentials: true, // 크로스 도메인 허용
+    methods: ["POST", "PATCH", "GET", "DELETE", "OPTIONS", "HEAD"],
+  })
+);
+
+// Content-Type: application/json 형태의 데이터를 인식하고 핸들링할 수 있게 함.
+app.use(express.json());
+
+// Content-Type: application/x-www-form-urlencoded 형태의 데이터를 인식하고 핸들링할 수 있게 함.
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.send("welcome!");
+});
+
+export { app };
