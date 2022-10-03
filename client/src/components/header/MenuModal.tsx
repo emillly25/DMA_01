07e6dc56badf15ddd4501e9352b6314f17726661
sehee <seeeh0128@gmail.com>
2022-10-03 22:57:30 +0900

    style: 사이드 메뉴 애니메이션 추가

--- client/src/components/header/MenuModal.tsx
+++ client/src/components/header/MenuModal.tsx
@@ -1,21 +1,49 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faX, faUser, faFilePen } from '@fortawesome/free-solid-svg-icons'
+import {
+  faBars,
+  faX,
+  faUser,
+  faFilePen,
+} from '@fortawesome/free-solid-svg-icons'
 import Image from 'next/image'
 import EngLogo from '../../assets/engLogo.png'
+import { useState } from 'react'
 
-export const MenuModal = ({ openModal }) => {
+export const MenuModal = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  function sideMenuController() {
+    setIsOpen((cur) => !cur)
+  }
   return (
     <>
+      {!isOpen ? (
+        <span
+          className="block md:hidden absolute right-2 top-7"
+          onClick={sideMenuController}
+        >
+          <FontAwesomeIcon
+            icon={faBars}
+            className="text-4xl text-lime-800 mr-5"
+          />
+        </span>
+      ) : (
+        <>
+          <div
+            onClick={sideMenuController}
+            className="absolute top-0 left-0 w-full h-screen bg-black opacity-30"
+          ></div>
+        </>
+      )}
       <div
-        onClick={openModal}
-        className="absolute top-0 left-0 w-full h-screen bg-black opacity-30"
-      ></div>
-      <div className="absolute top-0 right-0 bg-white h-screen w-3/4 text-white z-10 transition-all">
+        className={`fixed top-0 right-0 bg-white h-screen w-3/4 z-30 text-white  ${
+          isOpen ? 'translate-x-0' : 'translate-x-full'
+        } ease-in-out duration-500`}
+      >
         <div className="bg-white flex items-center justify-between">
           <div className="ml-3">
             <Image src={EngLogo} width={200} height={80} />
           </div>
-          <span onClick={openModal}>
+          <span onClick={sideMenuController}>
             <FontAwesomeIcon
               icon={faX}
               className="text-slate-600 text-3xl mr-4 "
@@ -41,7 +69,7 @@ export const MenuModal = ({ openModal }) => {
             </div>
           </div>
         </div>
-        <ul className="text-black text-2xl flex flex-col items-start font-bold ml-10">
+        <ul className="text-black text-lg flex flex-col items-start font-bold ml-10">
           <li className="mt-8 mb-4">DMA 소개</li>
           <li className="my-4">교육과정</li>
           <li className="my-4">입학안내</li>
