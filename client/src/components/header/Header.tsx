ce908c0beae8be31cec3c1fff72867e17731a207
sehee <seeeh0128@gmail.com>
2022-10-03 21:44:22 +0900

    style: 반응형 사이드 메뉴 디자인

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -12,34 +12,31 @@ export const Header = () => {
     setIsModal((cur) => !cur)
   }
   return (
-    <header className="text-gray-500 body-font">
-      {isModal && <MenuModal />}
-      <div className="container mx-auto relative p-10 bg-slate-100">
-        <a className="absolute left-2 top-2">
-          <Image src={Logo} width={200} height={70} />
+    <header className="container mx-auto relative p-10 bg-slate-100 text-gray-500">
+      <a className="absolute left-2 top-2">
+        <Image src={Logo} width={200} height={70} />
+      </a>
+      {isModal && <MenuModal openModal={openModal} />}
+      <nav className="md:ml-auto hidden md:block flex flex-wrap items-center text-lg justify-center absolute right-2 top-7">
+        <a className="mr-5 hover:text-gray-900">DMA 소개</a>
+        <a className="mr-5 hover:text-gray-900">교육과정</a>
+        <a className="mr-5 hover:text-gray-900">입학안내</a>
+        <a className="mr-5 hover:text-gray-900">DMA 소식</a>
+        <a className="mr-5 hover:text-gray-900">상담예약</a>
+        <a className="border-solid border-2 border-slate-400 px-2 py-1 rounded-full ">
+          <FontAwesomeIcon icon={faUser} className="text-xl text-black" />
         </a>
+      </nav>
 
-        <nav className="md:ml-auto hidden md:block flex flex-wrap items-center text-lg justify-center absolute right-2 top-7">
-          <a className="mr-5 hover:text-gray-900">DMA 소개</a>
-          <a className="mr-5 hover:text-gray-900">교육과정</a>
-          <a className="mr-5 hover:text-gray-900">입학안내</a>
-          <a className="mr-5 hover:text-gray-900">DMA 소식</a>
-          <a className="mr-5 hover:text-gray-900">상담예약</a>
-          <a className="border-solid border-2 border-slate-400 px-2 py-1 rounded-full ">
-            <FontAwesomeIcon icon={faUser} className="text-xl text-black" />
-          </a>
-        </nav>
-
-        <span
-          onClick={openModal}
-          className="block md:hidden absolute right-2 top-7"
-        >
-          <FontAwesomeIcon
-            icon={faBars}
-            className="text-4xl text-lime-800 mr-5"
-          />
-        </span>
-      </div>
+      <span
+        onClick={openModal}
+        className="block md:hidden absolute right-2 top-7"
+      >
+        <FontAwesomeIcon
+          icon={faBars}
+          className="text-4xl text-lime-800 mr-5"
+        />
+      </span>
     </header>
   )
 }
--- client/src/components/header/MenuModal.tsx
+++ client/src/components/header/MenuModal.tsx
@@ -1,3 +1,54 @@
-export const MenuModal = () => {
-  return <div>모달</div>
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faX, faUser, faFilePen } from '@fortawesome/free-solid-svg-icons'
+import Image from 'next/image'
+import EngLogo from '../../assets/engLogo.png'
+
+export const MenuModal = ({ openModal }) => {
+  return (
+    <>
+      <div
+        onClick={openModal}
+        className="absolute top-0 left-0 w-full h-screen bg-black opacity-30"
+      ></div>
+      <div className="absolute top-0 right-0 bg-white h-screen w-3/4 text-white z-10 transition-all">
+        <div className="bg-white flex items-center justify-between">
+          <div className="ml-3">
+            <Image src={EngLogo} width={200} height={80} />
+          </div>
+          <span onClick={openModal}>
+            <FontAwesomeIcon
+              icon={faX}
+              className="text-slate-600 text-3xl mr-4 "
+            />
+          </span>
+        </div>
+        <div className="bg-lime-800 flex flex-col text-white pt-5 pb-10">
+          <h3 className="text-center p-3 mb-2 font-bold text-lg ">
+            로그인이 필요합니다!
+          </h3>
+          <div className="flex justify-center gap-3 flex-wrap">
+            <div className="w-[100px] p-1 bg-white text-black rounded-sm flex justify-center">
+              <span>
+                <FontAwesomeIcon icon={faUser} className="text-black mx-1" />
+              </span>
+              로그인
+            </div>
+            <div className="w-[100px] p-1 bg-white text-black rounded-sm">
+              <span>
+                <FontAwesomeIcon icon={faFilePen} className="text-black mx-1" />
+              </span>
+              회원가입
+            </div>
+          </div>
+        </div>
+        <ul className="text-black text-2xl flex flex-col items-start font-bold ml-10">
+          <li className="mt-8 mb-4">DMA 소개</li>
+          <li className="my-4">교육과정</li>
+          <li className="my-4">입학안내</li>
+          <li className="my-4">DMA 소식</li>
+          <li className="my-4">상담예약</li>
+        </ul>
+      </div>
+    </>
+  )
 }
