8353676c8d1e0fdbee8e887dac6912ae307fbaad
SEHEE <89740574+emillly25@users.noreply.github.com>
2022-10-06 21:24:36 +0900

    Delete server/dist/src directory

--- server/dist/src/routers/loginRouter.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.loginRouter = void 0;
-const express_1 = require("express");
-const userController_1 = require("../controllers/userController");
-const loginRouter = (0, express_1.Router)();
-exports.loginRouter = loginRouter;
-loginRouter.get('/kakao', userController_1.userController.kakaoLogin);
--- server/dist/src/routers/userRouter.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userRouter = void 0;
-const express_1 = require("express");
-const userController_1 = require("../controllers/userController");
-const userRouter = (0, express_1.Router)();
-exports.userRouter = userRouter;
-userRouter.post('/users', userController_1.userController.add);
