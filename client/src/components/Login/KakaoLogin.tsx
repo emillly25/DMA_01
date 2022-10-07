378e837123d204c71b07b77cf57d56a266162dad
sehee <seeeh0128@gmail.com>
2022-10-07 14:37:12 +0900

    style: 로그인 페이지 화면구현

--- client/src/components/Login/KakaoLogin.tsx
+++ client/src/components/Login/KakaoLogin.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCommentDots } from '@fortawesome/free-solid-svg-icons'
 
 import KAKAO_IMG from '../../assets/kakao_login.png'
 
@@ -7,8 +9,15 @@ export default function KakaoLogin() {
   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_REDIRECT_URI}&response_type=code`
   return (
     <Link href={KAKAO_AUTH_URL}>
-      <div className="container mx-auto">
-        <Image src={KAKAO_IMG} />
+      <div className="w-[55%]">
+        <div className="flex gap-2 p-2 justify-center items-center bg-[#FEE500] text-[#000000] w-full h-[50px] rounded-md md:h-[60px]">
+          <span className="text-xl sm:text-2xl md:text-3xl">
+            <FontAwesomeIcon icon={faCommentDots} />
+          </span>
+          <p className="font-black text-sm sm:text-lg md:text-2xl">
+            카카오 로그인
+          </p>
+        </div>
       </div>
     </Link>
   )
--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,9 +1,30 @@
 import KakaoLogin from '../components/Login/KakaoLogin'
 import Layout from '../components/Layout'
+import Image from 'next/image'
+import smLogo from '../assets/smLogo.png'
+import Link from 'next/link'
 export default function Login() {
   return (
     <Layout>
-      <KakaoLogin />
+      <div className="container mx-auto p-3">
+        <h1 className="font-bold text-2xl text-center p-2 md:text-3xl">
+          로그인
+        </h1>
+        <div className="w-[90%] mx-auto border-slate-400 border-solid border-t-2 my-5 p-4">
+          <div className="flex items-center">
+            <div className="w-[40%] mx-auto">
+              <Image src={smLogo} alt="로고" />
+            </div>
+            <KakaoLogin />
+          </div>
+          <p className="text-center text-sm py-3 sm:text-base md:text-xl">
+            아직 회원이 아니신가요?
+            <Link href="/register">
+              <a className="text-gray-400 inline-block pl-3 "> 회원가입</a>
+            </Link>
+          </p>
+        </div>
+      </div>
     </Layout>
   )
 }
